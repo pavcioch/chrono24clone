389e2348f4b1e42598e52d467ce7e09aba0c0846
Pawel Chwaleba <pawel.chwaleba@pwc.com>
2018-03-11 21:35:19 +0000

    init version

---
+++ play/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am console app bitch !");
+
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+    }
+}
